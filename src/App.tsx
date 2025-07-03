ef03492768f80d37e562d5935088e960ad637073
Jamila <jamilaaliyeva32@gmail.com>
2025-07-03 23:06:52 +0500

    project initiated

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-sky-800">Hello</h1>
+      <Button variant="outline">Button</Button>
+    </>
+  );
+}
+
+export default App;
